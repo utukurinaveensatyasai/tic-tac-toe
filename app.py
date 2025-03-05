babe9aa66beaf7f56eea829a1b28fe8f68c15e35
Utukuri Naveen Satya Sai <132331077+utukurinaveensatyasai@users.noreply.github.com>
2025-03-05 12:57:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def reset():
     return jsonify({"message": "Game Reset"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
